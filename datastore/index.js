b6460724497068d08c44333c940a28132d82c7d1
Matthew Baseman <matthew.baseman@gmail.com>
2023-04-18 10:51:49 -0700

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -73,19 +73,15 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  console.log(id);
-
-  fs.unlink()
-
-
-// var item = items[id];
-//   delete items[id];
-//   if (!item) {
-//     // report an error if item not found
-//     callback(new Error(`No item with id: ${id}`));
-//   } else {
-//     callback();
-//   }
+  const toDoDir = `${exports.dataDir}/${id}.txt`;
+  fs.unlink(toDoDir, (err) => {
+    if (err) {
+      console.log('Error Removing File: ', err, id);
+      callback(err);
+    } else {
+      callback(null);
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
